60590d7411d0c5c2d44c9688424af4a81f7a8b67
shukhratovich-a <shukhratovich138@gmail.com>
2024-01-25 02:29:59 +0500

    Connect banner module

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 
 import { FileModule } from "./file/file.module";
+import { BannerModule } from "./poster/banner.module";
 import { CategoryModule } from "./category/category.module";
 import { SubcategoryModule } from "./subcategory/subcategory.module";
 import { ProductModule } from "./product/product.module";
 
-@Module({ imports: [FileModule, CategoryModule, SubcategoryModule, ProductModule] })
+@Module({ imports: [FileModule, BannerModule, CategoryModule, SubcategoryModule, ProductModule] })
 export class Modules {}
