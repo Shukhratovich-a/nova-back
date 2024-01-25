85fcdd709fcc2643687d3a030b6c5c7a0e0d70c7
shukhratovich-a <shukhratovich138@gmail.com>
2024-01-25 17:52:59 +0500

    Change module folder name

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 
 import { FileModule } from "./file/file.module";
-import { BannerModule } from "./poster/banner.module";
+import { BannerModule } from "./banner/banner.module";
 import { CategoryModule } from "./category/category.module";
 import { SubcategoryModule } from "./subcategory/subcategory.module";
 import { ProductModule } from "./product/product.module";
