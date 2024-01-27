94218f305f54bc259cdceb48b1a5a7a5995017f9
shukhratovich-a <shukhratovich138@gmail.com>
2024-01-28 01:36:11 +0500

    Delete unnecessary fields

--- src/modules/category/dtos/create-category.dto.ts
+++ src/modules/category/dtos/create-category.dto.ts
@@ -1,23 +1,19 @@
-import { IsEnum, IsNotEmpty, IsString } from "class-validator";
+import { IsEnum, IsString } from "class-validator";
 
 import { LanguageEnum } from "@enums/language.enum";
 
 export class CreateCategoryDto {
   @IsString()
-  @IsNotEmpty()
   poster: string;
 
   @IsString()
-  @IsNotEmpty()
   alias: string;
 }
 
 export class CreateCategoryContentDto {
   @IsString()
-  @IsNotEmpty()
   title: string;
 
   @IsEnum(LanguageEnum)
-  @IsNotEmpty()
   language: LanguageEnum;
 }
