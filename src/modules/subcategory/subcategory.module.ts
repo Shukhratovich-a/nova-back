8e0e580c7b17be6cac68a056de7635d45ca1600e
shukhratovich-a <shukhratovich138@gmail.com>
2024-01-28 01:39:14 +0500

    Connect service and controller

--- src/modules/subcategory/subcategory.module.ts
+++ src/modules/subcategory/subcategory.module.ts
@@ -4,14 +4,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { CategoryEntity, CategoryContentEntity } from "@modules/category/category.entity";
 import { ProductEntity, ProductContentEntity, ProductImageEntity } from "@modules/product/product.entity";
 
-// import { CategoryService } from '@modules/category/category.service';
-// import { ProductService } from '@modules/product/product.service';
+import { CategoryService } from "@modules/category/category.service";
+// import { ProductService } from "@modules/product/product.service";
 
 import { SubcategoryEntity, SubcategoryContentEntity } from "./subcategory.entity";
 
-// import { SubcategoryController } from './subcategory.controller';
+import { SubcategoryController } from "./subcategory.controller";
 
-// import { SubcategoryService } from './subcategory.service';
+import { SubcategoryService } from "./subcategory.service";
 
 @Module({
   imports: [
@@ -26,7 +26,7 @@ import { SubcategoryEntity, SubcategoryContentEntity } from "./subcategory.entit
       ProductImageEntity,
     ]),
   ],
-  // controllers: [SubcategoryController],
-  // providers: [SubcategoryService, CategoryService, ProductService],
+  controllers: [SubcategoryController],
+  providers: [SubcategoryService, CategoryService],
 })
 export class SubcategoryModule {}
