10eb3af68de0f43bd8ef0e423de3e21deb32d642
shukhratovich-a <shukhratovich138@gmail.com>
2024-01-28 01:37:56 +0500

    Create dto to create a new subcategory

---
+++ src/modules/subcategory/dtos/create-subcategory.dto.ts
@@ -0,0 +1,22 @@
+import { IsEnum, IsNumber, IsString } from "class-validator";
+
+import { LanguageEnum } from "@enums/language.enum";
+
+export class CreateSubcategoryDto {
+  @IsString()
+  poster: string;
+
+  @IsString()
+  alias: string;
+
+  @IsNumber()
+  categoryId: number;
+}
+
+export class CreateSubcategoryContentDto {
+  @IsString()
+  title: string;
+
+  @IsEnum(LanguageEnum)
+  language: LanguageEnum;
+}
