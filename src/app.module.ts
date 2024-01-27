fdaad62b21dfe684433631372b756e577aa87db0
shukhratovich-a <shukhratovich138@gmail.com>
2024-01-28 01:29:06 +0500

    Edit import aliases

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { getDatabaseConfig } from "@/configs/database.config";
+import { getDatabaseConfig } from "@configs/database.config";
 
-import { Modules } from "@/modules/modules.module";
+import { Modules } from "@modules/modules.module";
 
 @Module({
   imports: [
