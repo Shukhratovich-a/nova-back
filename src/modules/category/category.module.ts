55bc77080cd21af071d8e9be51b2bc74a190bfbd
shukhratovich-a <shukhratovich138@gmail.com>
2024-01-29 17:32:17 +0500

    Remove imported entities and import with forwardRef

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,32 +1,18 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { SubcategoryEntity, SubcategoryContentEntity } from "@modules/subcategory/subcategory.entity";
-import { ProductEntity, ProductContentEntity, ProductImageEntity } from "@modules/product/product.entity";
-
-// import { SubcategoryService } from "@modules/subcategory/subcategory.service";
-// import { ProductService } from "@modules/product/product.service";
+import { SubcategoryModule } from "@modules/subcategory/subcategory.module";
 
 import { CategoryEntity, CategoryContentEntity } from "./category.entity";
 
 import { CategoryController } from "./category.controller";
 
 import { CategoryService } from "./category.service";
-import { SubcategoryService } from "@modules/subcategory/subcategory.service";
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      CategoryEntity,
-      CategoryContentEntity,
-      SubcategoryEntity,
-      SubcategoryContentEntity,
-      ProductEntity,
-      ProductContentEntity,
-      ProductImageEntity,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([CategoryEntity, CategoryContentEntity]), forwardRef(() => SubcategoryModule)],
   controllers: [CategoryController],
-  providers: [CategoryService, SubcategoryService],
+  providers: [CategoryService],
+  exports: [CategoryService],
 })
 export class CategoryModule {}
