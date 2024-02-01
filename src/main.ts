a1d3d2251af37bc34abd92a54a90f9996c266ade
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-01 16:09:47 +0500

    Add whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 
 import { useContainer } from "class-validator";
 
@@ -6,11 +7,12 @@ import { AppModule } from "@/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(3001);
 }
 bootstrap();
-//
