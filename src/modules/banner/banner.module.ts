369b4b20de9653f519f055b473bdb16eff3dbbf2
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-06 15:22:06 +0500

    Remove deleted entity

--- src/modules/banner/banner.module.ts
+++ src/modules/banner/banner.module.ts
@@ -1,15 +1,16 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { BannerContentEntity, BannerEntity } from "./banner.entity";
+import { BannerEntity } from "./banner.entity";
 
 import { BannerController } from "./banner.controller";
 
 import { BannerService } from "./banner.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BannerEntity, BannerContentEntity])],
+  imports: [TypeOrmModule.forFeature([BannerEntity])],
   controllers: [BannerController],
   providers: [BannerService],
+  exports: [BannerService],
 })
 export class BannerModule {}
