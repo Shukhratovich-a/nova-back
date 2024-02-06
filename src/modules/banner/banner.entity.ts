5c062996965d28c254c4abf44d923ac9a2c46ed8
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-06 15:19:38 +0500

    Change localization mode

--- src/modules/banner/banner.entity.ts
+++ src/modules/banner/banner.entity.ts
@@ -1,17 +1,5 @@
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
-  Relation,
-  // CreateDateColumn,
-  // UpdateDateColumn,
-} from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-import { LanguageEnum } from "@enums/language.enum";
 import { StatusEnum } from "@enums/status.enum";
 
 @Entity("banners")
@@ -22,37 +10,48 @@ export class BannerEntity extends BaseEntity {
   @Column({ name: "poster", type: "varchar" })
   poster: string;
 
+  @Column({ name: "title_ru", type: "varchar", nullable: true })
+  titleRu: string;
+
+  @Column({ name: "title_en", type: "varchar", nullable: true })
+  titleEn: string;
+
+  @Column({ name: "title_tr", type: "varchar", nullable: true })
+  titleTr: string;
+
+  @Column({ name: "title_ar", type: "varchar", nullable: true })
+  titleAr: string;
+
+  @Column({ name: "description_ru", type: "varchar", nullable: true })
+  descriptionRu: string;
+
+  @Column({ name: "description_en", type: "varchar", nullable: true })
+  descriptionEn: string;
+
+  @Column({ name: "description_tr", type: "varchar", nullable: true })
+  descriptionTr: string;
+
+  @Column({ name: "description_ar", type: "varchar", nullable: true })
+  descriptionAr: string;
+
+  @Column({ name: "subtitle_ru", type: "varchar", nullable: true })
+  subtitleRu: string;
+
+  @Column({ name: "subtitle_en", type: "varchar", nullable: true })
+  subtitleEn: string;
+
+  @Column({ name: "subtitle_tr", type: "varchar", nullable: true })
+  subtitleTr: string;
+
+  @Column({ name: "subtitle_ar", type: "varchar", nullable: true })
+  subtitleAr: string;
+
   @Column({ name: "status", type: "simple-enum", enum: StatusEnum, default: "active" })
   status: StatusEnum;
 
-  @OneToMany(() => BannerContentEntity, (content) => content.banner, { onDelete: "CASCADE" })
-  contents: Relation<BannerContentEntity[]>;
+  @CreateDateColumn({ name: "create_at", type: "datetime" })
+  createAt: Date;
 
-  // @CreateDateColumn({ name: "create_at", type: "datetime" })
-  // createAt: Date;
-
-  // @UpdateDateColumn({ name: "update_at", type: "datetime" })
-  // updateAt: Date;
-}
-
-@Entity("banner_contents")
-export class BannerContentEntity extends BaseEntity {
-  @PrimaryGeneratedColumn({ name: "id" })
-  id: number;
-
-  @Column({ name: "title", type: "varchar" })
-  title: string;
-
-  @Column({ name: "description", type: "varchar", nullable: true })
-  description: string;
-
-  @Column({ name: "subtitle", type: "varchar", nullable: true })
-  subtitle: string;
-
-  @Column({ name: "language", type: "simple-enum", enum: LanguageEnum })
-  language: LanguageEnum;
-
-  @ManyToOne(() => BannerEntity, (banner) => banner.contents, { nullable: false })
-  @JoinColumn({ name: "banner_id" })
-  banner: BannerEntity;
+  @UpdateDateColumn({ name: "update_at", type: "datetime" })
+  updateAt: Date;
 }
