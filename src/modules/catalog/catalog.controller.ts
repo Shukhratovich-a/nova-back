9277f05445945ec78bab088368ca3810266f801a
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-09 00:00:01 +0500

    Edit catalog controller

--- src/modules/catalog/catalog.controller.ts
+++ src/modules/catalog/catalog.controller.ts
@@ -1,16 +1,4 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-  Put,
-  Query,
-  ValidationPipe,
-} from "@nestjs/common";
+import { Controller, Get, Post, Put, Delete, Body, Param, Query, ParseIntPipe, BadRequestException } from "@nestjs/common";
 
 import { EnumValidationPipe } from "@pipes/enum-validation.pipe";
 
@@ -35,38 +23,35 @@ export class CatalogController {
 
   // POST
   @Post("create")
-  async createCatalog(@Body(new ValidationPipe()) catalogDto: CreateCatalogDto) {
-    const catalog = await this.catalogService.checkCatalogForExist(catalogDto.year, catalogDto.language);
+  async create(@Body() catalogDto: CreateCatalogDto) {
+    const catalog = await this.catalogService.checkForExist(catalogDto.year, catalogDto.language);
     if (catalog) throw new BadRequestException("exists");
 
-    return this.catalogService.createCatalog(catalogDto);
+    return this.catalogService.create(catalogDto);
   }
 
   // PUT
   @Put("update/:catalogId")
-  async updateCatalog(
-    @Param("catalogId", new ParseIntPipe()) catalogId: number,
-    @Body(new ValidationPipe()) catalogDto: UpdateCatalogDto,
-  ) {
-    const catalog = await this.catalogService.checkCatalogById(catalogId);
+  async update(@Param("catalogId", new ParseIntPipe()) catalogId: number, @Body() catalogDto: UpdateCatalogDto) {
+    const catalog = await this.catalogService.checkById(catalogId);
     if (!catalog) throw new BadRequestException("not found");
 
-    const oldCatalog = await this.catalogService.checkCatalogForExistById(
+    const oldCatalog = await this.catalogService.checkForExistById(
       catalogId,
       catalogDto.year || catalog.year,
       catalogDto.language || catalog.language,
     );
     if (oldCatalog) throw new BadRequestException("exists");
 
-    return this.catalogService.updateCatalog(catalogDto, catalogId);
+    return this.catalogService.update(catalogDto, catalogId);
   }
 
   // DELETE
   @Delete("delete/:catalogId")
-  async deleteCatalog(@Param("catalogId", new ParseIntPipe()) catalogId: number) {
-    const catalog = await this.catalogService.checkCatalogById(catalogId);
+  async delete(@Param("catalogId", new ParseIntPipe()) catalogId: number) {
+    const catalog = await this.catalogService.checkById(catalogId);
     if (!catalog) throw new BadRequestException("not found");
 
-    return this.catalogService.deleteCatalog(catalogId);
+    return this.catalogService.delete(catalogId);
   }
 }
