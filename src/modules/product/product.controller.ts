dbd2a911e943879a53d07d0696f22ffb28ee1968
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-09 07:33:23 +0500

    Edit product controller

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,15 +1,4 @@
-import {
-  Body,
-  Controller,
-  Param,
-  Query,
-  Get,
-  Post,
-  ValidationPipe,
-  Put,
-  ParseIntPipe,
-  BadRequestException,
-} from "@nestjs/common";
+import { Body, Controller, Param, Query, Get, Post, Put, ParseIntPipe, BadRequestException, Delete } from "@nestjs/common";
 
 import { EnumValidationPipe } from "@pipes/enum-validation.pipe";
 
@@ -20,15 +9,12 @@ import { StatusEnum } from "@enums/status.enum";
 import { SubcategoryService } from "@modules/subcategory/subcategory.service";
 import { ProductService } from "./product.service";
 
-import { CreateProductDto, CreateProductContentDto, CreateProductImageDto } from "./dtos/create-product.dto";
-import { UpdateProductDto, UpdateProductContentDto, UpdateProductImageDto } from "./dtos/update-product.dto";
+import { CreateProductDto } from "./dtos/create-product.dto";
+import { UpdateProductDto } from "./dtos/update-product.dto";
 
 @Controller("product")
 export class ProductController {
-  constructor(
-    private readonly subcategoryService: SubcategoryService,
-    private readonly productService: ProductService,
-  ) {}
+  constructor(private readonly subcategoryService: SubcategoryService, private readonly productService: ProductService) {}
 
   // GET
   @Get("get-all")
@@ -49,77 +35,64 @@ export class ProductController {
     return this.productService.findById(productId, language, status);
   }
 
+  @Get("get-with-count")
+  async getAllWithCount(
+    @Query("status", new EnumValidationPipe(StatusEnum, { defaultValue: StatusEnum.ACTIVE })) status: StatusEnum,
+    @Query() { page, limit }: IPagination,
+  ) {
+    return this.productService.findAllWithCount(status, { page, limit });
+  }
+
+  @Get("get-one-with-contents/:productId")
+  async getOneWithContents(
+    @Query("status", new EnumValidationPipe(StatusEnum, { defaultValue: StatusEnum.ACTIVE })) status: StatusEnum,
+    @Param("productId", new ParseIntPipe()) productId: number,
+  ) {
+    return this.productService.findOneWithContents(productId, status);
+  }
+
+  @Get("get-by-parent/:subcategoryId")
+  async getAllByParentId(
+    @Query("status", new EnumValidationPipe(StatusEnum, { defaultValue: StatusEnum.ACTIVE })) status: StatusEnum,
+    @Query() { page, limit }: IPagination,
+    @Param("subcategoryId", new ParseIntPipe()) subcategoryId: number,
+  ) {
+    return this.productService.findAllByParentId(subcategoryId, status, { page, limit });
+  }
+
   // POST
-  @Post("create-product")
-  async createProduct(@Body(new ValidationPipe()) productDto: CreateProductDto) {
-    const subcategory = await this.subcategoryService.checkSubcategoryById(productDto.subcategoryId);
+  @Post("create")
+  async create(@Body() productDto: CreateProductDto) {
+    const subcategory = await this.subcategoryService.checkById(productDto.subcategoryId);
     if (!subcategory) throw new BadRequestException("category not exists");
 
-    const product = await this.productService.checkProductByCode(productDto.code);
+    const product = await this.productService.checkByCode(productDto.code);
     if (product) throw new BadRequestException("product exists");
 
-    return this.productService.createProduct(productDto);
+    return this.productService.create(productDto);
   }
 
-  @Post("create-content/:productId")
-  async createContent(
-    @Param("productId", new ParseIntPipe()) productId: number,
-    @Body(new ValidationPipe()) contentDto: CreateProductContentDto,
-  ) {
-    const product = await this.productService.checkProductById(productId);
-    if (!product) throw new BadRequestException("not found");
+  // POST
+  @Put("update/:productId")
+  async update(@Param("productId", new ParseIntPipe()) productId: number, @Body() productDto: UpdateProductDto) {
+    const productById = await this.productService.checkById(productId);
+    if (!productById) throw new BadRequestException("not found");
 
-    const oldContent = await this.productService.checkContentForExist(productId, contentDto.language);
-    if (oldContent) throw new BadRequestException("exists");
-
-    return this.productService.createContent(contentDto, productId);
-  }
-
-  @Post("create-image/:productId")
-  async createImage(
-    @Param("productId", new ParseIntPipe()) productId: number,
-    @Body(new ValidationPipe()) imageDto: CreateProductImageDto,
-  ) {
-    const product = await this.productService.checkProductById(productId);
-    if (!product) throw new BadRequestException("not found");
-
-    const oldImage = await this.productService.checkImageForExist(productId, imageDto.type);
-    if (oldImage) throw new BadRequestException("exists");
-
-    return this.productService.createImage(imageDto, productId);
-  }
-
-  // PUT
-  @Put("update-product/:productId")
-  async updateProduct(
-    @Param("productId", new ParseIntPipe()) productId: number,
-    @Body(new ValidationPipe()) productDto: UpdateProductDto,
-  ) {
-    const product = await this.productService.checkProductById(productId);
-    if (!product) throw new BadRequestException("product not exists");
-
-    const subcategory = await this.subcategoryService.checkSubcategoryById(productDto.subcategoryId);
+    const subcategory = await this.subcategoryService.checkById(productDto.subcategoryId);
     if (!subcategory) throw new BadRequestException("category not exists");
 
-    return this.productService.updateProduct(productDto, productId);
+    const product = await this.productService.checkByCode(productDto.code);
+    if (product) throw new BadRequestException("product exists");
+
+    return this.productService.update(productDto, productId);
   }
 
-  @Put("update-content/:contentId")
-  async updateContent(
-    @Param("contentId", new ParseIntPipe()) contentId: number,
-    @Body(new ValidationPipe()) contentDto: UpdateProductContentDto,
-  ) {
-    const content = await this.productService.checkContentById(contentId);
-    if (!content) throw new BadRequestException("not found");
+  // DELETE
+  @Delete("delete/:productId")
+  async delete(@Param("productId", new ParseIntPipe()) productId: number) {
+    const product = await this.productService.checkById(productId);
+    if (!product) throw new BadRequestException("not found");
 
-    return this.productService.updateContent(contentDto, contentId);
-  }
-
-  @Put("update-image/:imageId")
-  async updateImage(@Param("imageId", new ParseIntPipe()) imageId: number, @Body() imageDto: UpdateProductImageDto) {
-    const image = await this.productService.checkImageById(imageId);
-    if (!image) throw new BadRequestException("not found");
-
-    return this.productService.updateImage(imageDto, imageId);
+    return this.productService.delete(productId);
   }
 }
