8a72ae715ce54ed1c4364ecbf8bfd7f7e78a7430
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-09 07:31:15 +0500

    Edit detail service

--- src/modules/detail/detail.service.ts
+++ src/modules/detail/detail.service.ts
@@ -4,49 +4,213 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { plainToClass } from "class-transformer";
 
+import { IPagination } from "@interfaces/pagination.interface";
 import { LanguageEnum } from "@enums/language.enum";
+import { StatusEnum } from "@enums/status.enum";
 
-import { DetailContentEntity, DetailEntity, DetailCategoryEntity, DetailCategoryContentEntity } from "./detail.entity";
+import { capitalize } from "@utils/capitalize.utils";
+
+import { DetailEntity, DetailCategoryEntity, DetailTypeEntity } from "./detail.entity";
 
 import { DetailCategoryDto, DetailDto } from "./dtos/detail.dto";
-import { CreateDetailDto, CreateDetailContentDto } from "./dtos/create-detail.dto";
+import { CreateDetailDto, CreateDetailCategoryDto, CreateDetailTypeDto } from "./dtos/create-detail.dto";
+import { UpdateDetailDto, UpdateDetailCategoryDto, UpdateDetailTypeDto } from "./dtos/update-detail.dto";
 
+// DETAIL CATEGORY
+@Injectable()
+export class DetailCategoryService {
+  constructor(
+    @InjectRepository(DetailCategoryEntity) private readonly detailCategoryRepository: Repository<DetailCategoryEntity>,
+  ) {}
+
+  // FIND
+  async findAll() {
+    return this.detailCategoryRepository.find({ where: { status: StatusEnum.ACTIVE } });
+  }
+
+  async findAllWithCount(status: StatusEnum, { page, limit }: IPagination) {
+    const [categories, total] = await this.detailCategoryRepository.findAndCount({
+      where: { status },
+      take: limit,
+      skip: (page - 1) * limit || 0,
+    });
+    if (!categories) return [];
+
+    return { data: categories, total };
+  }
+
+  async findOneWithContents(categoryId: number, status: StatusEnum) {
+    const category = await this.detailCategoryRepository.findOne({
+      where: { status, id: categoryId },
+    });
+    if (!category) return null;
+
+    return category;
+  }
+
+  // CREATE
+  async create(categoryDto: CreateDetailCategoryDto) {
+    return await this.detailCategoryRepository.save(this.detailCategoryRepository.create({ ...categoryDto }));
+  }
+
+  // UPDATE
+  async update(categoryDto: UpdateDetailCategoryDto, categoryId: number) {
+    return await this.detailCategoryRepository.save({ ...categoryDto, id: categoryId });
+  }
+
+  // DELETE
+  async delete(categoryId: number) {
+    return await this.detailCategoryRepository.save({ status: StatusEnum.DELETED, id: categoryId });
+  }
+
+  // CHECKERS
+  async checkById(categoryId: number) {
+    return this.detailCategoryRepository.findOne({ where: { id: categoryId } });
+  }
+
+  // PARSERS
+  parse(category: DetailCategoryEntity, language: LanguageEnum) {
+    const newCategory: DetailCategoryDto = plainToClass(DetailCategoryDto, category, { excludeExtraneousValues: true });
+
+    newCategory.title = category[`title${capitalize(language)}`];
+
+    return newCategory;
+  }
+}
+
+// DETAIL TYPE
+@Injectable()
+export class DetailTypeService {
+  constructor(@InjectRepository(DetailTypeEntity) private readonly detailTypeRepository: Repository<DetailTypeEntity>) {}
+
+  // FIND
+  async findAll() {
+    return this.detailTypeRepository.find({ where: { status: StatusEnum.ACTIVE } });
+  }
+
+  async findAllWithCount(status: StatusEnum, { page, limit }: IPagination) {
+    const [types, total] = await this.detailTypeRepository.findAndCount({
+      where: { status },
+      take: limit,
+      skip: (page - 1) * limit || 0,
+    });
+    if (!types) return [];
+
+    return { data: types, total };
+  }
+
+  async findOneWithContents(typeId: number, status: StatusEnum) {
+    const type = await this.detailTypeRepository.findOne({
+      where: { status, id: typeId },
+    });
+    if (!type) return null;
+
+    return type;
+  }
+
+  // CREATE
+  async create(typeDto: CreateDetailTypeDto) {
+    return await this.detailTypeRepository.save(this.detailTypeRepository.create({ ...typeDto }));
+  }
+
+  // UPDATE
+  async update(typeDto: UpdateDetailTypeDto, typeId: number) {
+    return await this.detailTypeRepository.save({ ...typeDto, id: typeId });
+  }
+
+  // DELETE
+  async delete(typeId: number) {
+    return await this.detailTypeRepository.save({ status: StatusEnum.DELETED, id: typeId });
+  }
+
+  // CHECKERS
+  async checkById(typeId: number) {
+    return this.detailTypeRepository.findOne({ where: { id: typeId } });
+  }
+}
+
+// DETAIL
 @Injectable()
 export class DetailService {
   constructor(
     @InjectRepository(DetailEntity) private readonly detailRepository: Repository<DetailEntity>,
-    @InjectRepository(DetailContentEntity) private readonly detailContentRepository: Repository<DetailContentEntity>,
-    @InjectRepository(DetailCategoryEntity) private readonly detailCategoryRepository: Repository<DetailCategoryEntity>,
-    @InjectRepository(DetailCategoryContentEntity)
-    private readonly detailCategoryContentRepository: Repository<DetailCategoryContentEntity>,
+    private readonly detailCategoryService: DetailCategoryService,
   ) {}
 
   // FIND
-  async findAllCategories(language: LanguageEnum) {
-    return this.detailCategoryRepository.find({ relations: { contents: true }, where: { contents: { language } } });
+  async findAllWithCount(status: StatusEnum, { page, limit }: IPagination) {
+    const [details, total] = await this.detailRepository.findAndCount({
+      relations: { type: true, category: true },
+      where: { status },
+      take: limit,
+      skip: (page - 1) * limit || 0,
+    });
+    if (!details) return [];
+
+    return { data: details, total };
   }
 
-  async findDetailsById(productId: number, language: LanguageEnum) {
-    return this.detailRepository.find({
-      relations: { contents: true, category: { contents: true } },
-      where: { product: { id: productId }, contents: { language } },
+  async findOneWithContents(detailId: number, status: StatusEnum) {
+    const detail = await this.detailRepository.findOne({
+      relations: { type: true, category: true },
+      where: { status, id: detailId },
     });
+    if (!detail) return null;
+
+    return detail;
+  }
+
+  async findAllByParentId(productId: number, status: StatusEnum, { page, limit }: IPagination) {
+    const [details, total] = await this.detailRepository.findAndCount({
+      relations: { type: true, category: true },
+      where: { status, product: { id: productId } },
+      order: { category: { id: "ASC" } },
+      take: limit,
+      skip: (page - 1) * limit || 0,
+    });
+    if (!details) return [];
+
+    return { data: details, total };
   }
 
   // CREATE
-  async createDetail(detailDto: CreateDetailDto) {
-    return this.detailRepository.save(
-      this.detailRepository.create({ product: { id: detailDto.productId }, category: { id: detailDto.categoryId } }),
+  async create(detailDto: CreateDetailDto) {
+    return await this.detailRepository.save(
+      this.detailRepository.create({
+        ...detailDto,
+        product: { id: detailDto.productId },
+        type: { id: detailDto.typeId },
+        category: { id: detailDto.categoryId },
+      }),
     );
   }
 
-  async createDetailContent(detailContentDto: CreateDetailContentDto, detailId: number) {
-    return this.detailContentRepository.save(
-      this.detailContentRepository.create({
-        ...detailContentDto,
-        detail: { id: detailId },
-      }),
-    );
+  // UPDATE
+  async update(detailDto: UpdateDetailDto, detailId: number) {
+    return await this.detailRepository.save({ ...detailDto, id: detailId });
+  }
+
+  // DELETE
+  async delete(typeId: number) {
+    return await this.detailRepository.save({ status: StatusEnum.DELETED, id: typeId });
+  }
+
+  // CHECKERS
+  async checkById(detailId: number) {
+    return this.detailRepository.findOne({ where: { id: detailId } });
+  }
+
+  // PARSERS
+  parse(detail: DetailEntity, language: LanguageEnum) {
+    const newDetail: DetailDto = plainToClass(DetailDto, detail, { excludeExtraneousValues: true });
+
+    if (detail.type) {
+      newDetail.title = detail.type[`title${capitalize(language)}`];
+    } else {
+      newDetail.title = null;
+    }
+
+    return newDetail;
   }
 
   // ASSETS
@@ -54,46 +218,27 @@ export class DetailService {
     if (!details || !details.length) return [];
 
     const newDetails: DetailCategoryDto[] = [];
-    const detailCategories = await this.findAllCategories(language);
+    const detailCategories = await this.detailCategoryService.findAll();
 
     detailCategories.forEach((category) => {
-      const filteredDetails = details.filter((detail) => detail.category.id === category.id);
+      const filteredDetails = details.filter((detail) => detail.category && detail.category.id === category.id);
 
       if (filteredDetails.length) {
-        const newCategory: DetailCategoryDto = {
-          id: category.id,
-          title: category.contents[0].title,
-          details: filteredDetails.map((detail) => this.parseDetail(detail)),
-        };
+        const newCategory: DetailCategoryDto = this.detailCategoryService.parse(category, language);
+        newCategory.details = filteredDetails.map((detail) => this.parse(detail, language));
 
         newDetails.push(newCategory);
       }
     });
 
+    const otherDetails = details.filter((detail) => !detail.category);
+    const otherCategory: DetailCategoryDto = {
+      id: null,
+      title: "other",
+      details: otherDetails.map((detail) => this.parse(detail, language)),
+    };
+    newDetails.push(otherCategory);
+
     return newDetails;
-  }
-
-  // PARSERS
-  parseDetail(detail: DetailEntity) {
-    const newDetail: DetailDto = plainToClass(DetailDto, detail, { excludeExtraneousValues: true });
-
-    if (detail.contents && detail.contents.length) {
-      newDetail.name = detail.contents[0].name;
-      newDetail.value = detail.contents[0].value;
-    }
-
-    return newDetail;
-  }
-
-  parseDetailCategory(category: DetailCategoryEntity) {
-    const newCategory: DetailCategoryDto = plainToClass(DetailCategoryDto, category, {
-      excludeExtraneousValues: true,
-    });
-
-    if (category.contents && category.contents.length) {
-      newCategory.title = category.contents[0].title;
-    }
-
-    return newCategory;
   }
 }
