60451e818f7dce4f8bbf3a636ee65cb1c960e38d
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-10 19:03:41 +0500

    Correct file upload

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { ServeStaticModule } from "@nestjs/serve-static";
-import { MulterModule } from "@nestjs/platform-express";
 
 import { join } from "path";
 import { path } from "app-root-path";
@@ -15,8 +14,6 @@ import { FileService } from "./file.service";
       rootPath: join(path, "uploads"),
       serveRoot: "/uploads",
     }),
-
-    MulterModule.register({ dest: join(path, "uploads") }),
   ],
   controllers: [FileController],
   providers: [FileService],
