24cfd2aaa0f77cea85be95ab1b08b4bbb513123a
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-10 09:13:12 +0500

    Edit logs

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -13,16 +13,20 @@ import { MFile } from "./mfile.class";
 @Injectable()
 export class FileService {
   async saveFile(file: MFile): Promise<FileElementResponse> {
-    const dateFolder = format(new Date(), "yyyy-MM-dd");
-    const uploadFolder = join(path, "uploads", dateFolder);
-    await ensureDir(uploadFolder);
+    try {
+      const dateFolder = format(new Date(), "yyyy-MM-dd");
+      const uploadFolder = join(path, "uploads", dateFolder);
+      await ensureDir(uploadFolder);
 
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const ext = extname(file.originalname);
-    const filename = `${file.originalname}-${uniqueSuffix}${ext}`;
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      const ext = extname(file.originalname);
+      const filename = `${file.originalname}-${uniqueSuffix}${ext}`;
 
-    await writeFile(join(uploadFolder, filename), file.buffer);
-    return { url: `/uploads/${dateFolder}/${filename}`, name: file.originalname };
+      await writeFile(join(uploadFolder, filename), file.buffer);
+      return { url: `/uploads/${dateFolder}/${filename}`, name: file.originalname };
+    } catch (error) {
+      return error;
+    }
   }
 
   convertToWebp(file: Buffer): Promise<Buffer> {
