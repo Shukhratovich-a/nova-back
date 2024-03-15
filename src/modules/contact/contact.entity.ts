94555d6fecc6fe176ef6d0e55419e4633d5ba939
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-15 17:47:51 +0500

    Remove coords

--- src/modules/contact/contact.entity.ts
+++ src/modules/contact/contact.entity.ts
@@ -1,14 +1,4 @@
-import {
-  Entity,
-  BaseEntity,
-  PrimaryGeneratedColumn,
-  Column,
-  OneToOne,
-  JoinColumn,
-  Relation,
-  CreateDateColumn,
-  UpdateDateColumn,
-} from "typeorm";
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
 import { StatusEnum } from "@enums/status.enum";
 import { ContactTypeEnum } from "@enums/contact-type.enum";
@@ -36,33 +26,11 @@ export class ContactEntity extends BaseEntity {
   @Column({ name: "city", type: "varchar", nullable: true })
   city: string;
 
+  @Column({ name: "map", type: "varchar" })
+  map: string;
+
   @Column({ name: "status", type: "simple-enum", enum: StatusEnum, default: "active" })
   status: StatusEnum;
-
-  @OneToOne(() => CoordEntity, (map) => map.contact, { onDelete: "CASCADE" })
-  coord: Relation<CoordEntity>;
-
-  @CreateDateColumn({ name: "create_at", type: "datetime" })
-  createAt: Date;
-
-  @UpdateDateColumn({ name: "update_at", type: "datetime" })
-  updateAt: Date;
-}
-
-@Entity("coords")
-export class CoordEntity extends BaseEntity {
-  @PrimaryGeneratedColumn({ name: "id" })
-  id: number;
-
-  @Column({ name: "longitude", type: "varchar" })
-  longitude: string;
-
-  @Column({ name: "latitude", type: "varchar" })
-  latitude: string;
-
-  @OneToOne(() => ContactEntity, (contact) => contact.coord, { onDelete: "CASCADE" })
-  @JoinColumn()
-  contact: Relation<ContactEntity>;
 
   @CreateDateColumn({ name: "create_at", type: "datetime" })
   createAt: Date;
