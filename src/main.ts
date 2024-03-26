cf5c5c305b7b107e4c1d6f5c297ead7fb7e6dc1e
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-26 23:55:19 +0500

    Test image upload

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ import { AppModule } from "@/app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ["https://novaplastik.uz", "https://nova-front.vercel.app", "https://admin.novaplastik.uz", "http://localhost:5173"],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
