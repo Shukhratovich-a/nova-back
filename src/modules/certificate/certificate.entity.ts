ce425266b3229af454b7273d3ef416fb3fee918d
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-02 23:16:06 +0500

    Add order and localization

--- src/modules/certificate/certificate.entity.ts
+++ src/modules/certificate/certificate.entity.ts
@@ -1,18 +1,30 @@
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-@Entity("certificates")
+@Entity("certificates", { orderBy: { order: "ASC" } })
 export class CertificateEntity extends BaseEntity {
   @PrimaryGeneratedColumn({ name: "id" })
   id: number;
 
-  @Column({ name: "title", type: "varchar" })
-  title: string;
+  @Column({ name: "title_en", type: "varchar", nullable: true })
+  titleEn: string;
+
+  @Column({ name: "title_ru", type: "varchar", nullable: true })
+  titleRu: string;
+
+  @Column({ name: "title_tr", type: "varchar", nullable: true })
+  titleTr: string;
+
+  @Column({ name: "title_ar", type: "varchar", nullable: true })
+  titleAr: string;
 
   @Column({ name: "poster", type: "varchar" })
   poster: string;
 
   @Column({ name: "certificate", type: "varchar" })
   certificate: string;
+
+  @Column({ name: "order", type: "int", default: 1000 })
+  order: number;
 
   @CreateDateColumn({ name: "create_at", type: "datetime" })
   createAt: Date;
