3732611b57c483db5762e4624bebf1694a682590
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-02 23:21:14 +0500

    Add order function and create parser

--- src/modules/certificate/certificate.service.ts
+++ src/modules/certificate/certificate.service.ts
@@ -1,25 +1,33 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 
 import { Repository } from "typeorm";
+import { plainToClass } from "class-transformer";
 
+import { LanguageEnum } from "@/enums/language.enum";
 import { IPagination } from "@interfaces/pagination.interface";
+
+import { capitalize } from "@/utils/capitalize.utils";
 
 import { CertificateEntity } from "./certificate.entity";
 
+import { CertificateDto } from "./dtos/certificate.dto";
 import { CreateCertificateDto } from "./dtos/create-certificate.dto";
 import { UpdateCertificateDto } from "./dtos/update-certificate.dto";
+import { OrderCertificateDto } from "./dtos/order-certificate.dto";
 
 @Injectable()
 export class CertificateService {
   constructor(@InjectRepository(CertificateEntity) private readonly certificateRepository: Repository<CertificateEntity>) {}
 
   // FIND
-  async findAll({ page = 1, limit = 0 }: IPagination) {
-    return await this.certificateRepository.find({
-      take: limit,
-      skip: (page - 1) * limit,
-    });
+  async findAll({ page = 1, limit = 0 }: IPagination, language?: LanguageEnum) {
+    const certificates = await this.certificateRepository.find({ take: limit, skip: (page - 1) * limit });
+    if (!certificates) return [];
+
+    const parsedCertificates: CertificateDto[] = certificates.map((certificate) => this.parse(certificate, language));
+
+    return parsedCertificates;
   }
 
   async findAllWithCount({ page = 1, limit = 0 }: IPagination) {
@@ -52,6 +60,22 @@ export class CertificateService {
     return certificate;
   }
 
+  // ORDER
+  async order(certificates: OrderCertificateDto[]) {
+    try {
+      for (const { id, order } of certificates) {
+        const currentCertificate = await this.certificateRepository.find({ where: { id } });
+        if (!currentCertificate) continue;
+
+        await this.certificateRepository.save({ order, id });
+      }
+
+      return { success: true };
+    } catch {
+      throw new BadRequestException();
+    }
+  }
+
   // CREATE
   async create(certificateDto: CreateCertificateDto) {
     return await this.certificateRepository.save(this.certificateRepository.create({ ...certificateDto }));
@@ -70,5 +94,14 @@ export class CertificateService {
   // CHECKERS
   async checkCertificateById(certificateId: number) {
     return this.certificateRepository.findOne({ where: { id: certificateId } });
+  }
+
+  // PARSERS
+  parse(about: CertificateEntity, language: LanguageEnum) {
+    const newAbout: CertificateDto = plainToClass(CertificateDto, about, { excludeExtraneousValues: true });
+
+    newAbout.title = about[`title${capitalize(language)}`];
+
+    return newAbout;
   }
 }
