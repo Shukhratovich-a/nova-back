99ab05639db365fab2f74db7a4a78b2353f57280
shukhratovich-a <shukhratovich138@gmail.com>
2024-08-10 03:59:32 +0500

    refactor: remove unnecessary configs

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,30 +1,14 @@
-import { ConfigService } from "@nestjs/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 import { join } from "path";
 
-export const getDatabaseConfig = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
+export const getDatabaseConfig = async (): Promise<TypeOrmModuleOptions> => {
   return {
     type: "sqlite",
-    database: join(process.cwd(), "database", "db.sqlite"),
-    // type: "postgres",
-    // url: getDatabaseString(configService),
+    database: join(process.cwd(), "database", `db.sqlite`),
     ...getDatabaseOptions(),
   };
 };
-
-export const getDatabaseString = (configService: ConfigService) =>
-  configService.get("postgresql") +
-  "://" +
-  configService.get("DB_USERNAME") +
-  ":" +
-  configService.get("DB_PASSWORD") +
-  "@" +
-  configService.get("DB_HOST") +
-  ":" +
-  configService.get("DB_PORT") +
-  "/" +
-  configService.get("DB_DATABASE");
 
 const getDatabaseOptions = () => ({
   useNewUrlParser: true,
