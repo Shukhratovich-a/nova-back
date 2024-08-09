179e71f6de84aaaf232242551d5fd2f8ad7c7bea
shukhratovich-a <shukhratovich138@gmail.com>
2024-08-10 03:58:02 +0500

    refactor: remove unnecessary hosts

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,7 @@ import { AppModule } from "@/app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ["error", "verbose", "warn", "debug"] });
 
-  app.enableCors({
-    origin: ["https://novaplastik.uz", "https://nova-front.vercel.app", "https://admin.novaplastik.uz", "http://localhost:5173"],
-  });
+  app.enableCors({ origin: ["https://novaplastik.uz", "https://nova-front.vercel.app", "https://admin.novaplastik.uz"] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
