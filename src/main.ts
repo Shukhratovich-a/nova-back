4eae7701ac36145b3cf50f5db2c17786c2da7467
shukhratovich-a <shukhratovich138@gmail.com>
2025-02-16 19:31:04 +0500

    refactor: test

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { useContainer } from "class-validator";
 
 import { AppModule } from "@/app.module";
 
-import { corsList } from "./constants/cors.constant";
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ["error", "verbose", "warn", "debug"] });
 
@@ -14,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  console.log(parseInt(process.env.PORT, 10));
+  console.log(process.env.PORT);
   console.log(process.env.HOSTNAME);
 
   await app.listen(parseInt(process.env.PORT, 10) || 3001);
